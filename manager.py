1b5ccedd80933d5484cd856e7408b2c3ed141707
Kerem <kerem0comert@gmail.com>
2020-04-22 17:17:34 +0300

    added networking stuff

--- manager.py
+++ manager.py
@@ -1,12 +1,42 @@
 import tkinter as tk
 import tkinter.messagebox as mb #messagebox doesn't get imported implicitly by the above import 
+import socket
 
 
 
+def initNetwork():
+    HOST = 'localhost'
+    PORT = 5000
+
+    print("Attempting connection...")
+    mySocket = socket.socket(socket.AF_INET, #for ipv4 communiciation
+                            socket.SOCK_STREAM # TCP Protocol
+                            )
+
+    try: mySocket.connect((HOST,PORT))
+    except: mb.showerror("Error", "Couldn't connect to the server.")
+    print("Connected to server.")
+    serverResponse = mySocket.recv(1024).decode()
+    while serverResponse != "SERVER >>> TERMINATE":
+        print(serverResponse)
+        message = input("CLIENT >>> ")
+        message = ("CLIENT >>> " + message).encode()
+        mySocket.send(message)
+        serverResponse = mySocket.recv(1024).decode()
+    
+    message = "CLIENT >>> TERMINATE".encode()
+    mySocket.send()
+    print("Connection terminated")
+    mySocket.close()
+
+#------------GUI
 
 root = tk.Tk()   #initialize the tkinter window
-root.title("---MANAGER LOGIN---")
-root.geometry("300x300")
+root.title("Manager Login")
+root.geometry("250x80")
+root.eval('tk::PlaceWindow %s center' % root.winfo_pathname(
+                root.winfo_id())) #center the window when created
+
 
 labelUsername = tk.Label(root, text="Username: ").grid(row=0,column=0)
 labelPassword = tk.Label(root, text= "Password: ").grid(row=1,column=0)
@@ -22,14 +52,16 @@ entryPassword.grid(row=1,column=1)
 def onLoginClick():
     username = entryUsername.get()
     password = entryPassword.get() 
-    mb.showinfo("Information","Informative message")
+    if not username: mb.showerror("Error", "Username cannot be empty!")
+    if not password: mb.showerror("Error", "Password cannot be empty!")
+    else: 
+        mb.showinfo("Information","Informative message")
 
-buttonLogin = tk.Button(root, text="Login",bg="blue",fg="white", 
+buttonLogin = tk.Button(root,text="Login",bg="blue",fg="white", 
                             command = onLoginClick).grid(row=2,column=1)
 
-
-
-
+initNetwork()
 root.mainloop() #this should be called after all the inits
+
 
 
--- server.py
+++ server.py
@@ -1,1 +1,34 @@
 import sqlite3
+import socket
+
+HOST = 'localhost'
+PORT = 5000
+
+
+serverSocket = socket.socket(socket.AF_INET, #for ipv4 communiciation
+                        socket.SOCK_STREAM # TCP Protocol
+                        )
+
+serverSocket.bind((HOST,PORT))
+
+while 1:
+    print("Waiting for connection...")
+    serverSocket.listen()
+    connection, address = serverSocket.accept()
+
+    print("Connection is recieved from, ", address[0])
+    
+    message = "SERVER >>> Connection successful".encode()
+    connection.send(message)
+
+    clientResponse = connection.recv(1024).decode()
+    while clientResponse != "CLIENT >>> TERMINATE":
+        print(clientResponse)
+        message = input("SERVER >>> ")
+        message = ("SERVER >>> " + message).encode()
+        connection.send(message)
+        clientResponse = connection.recv(1024).decode()
+    message = "SERVER >>> Successfully terminated".encode()
+    connection.send(message)
+    connection.close()
+    print("Connection terminated successfully.")
