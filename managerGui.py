746233f52531af2f2a0b1c31884d30d998a04c1e
KasliSoykan <58806238+KasliSoykan@users.noreply.github.com>
2020-05-02 11:59:39 +0300

    Update managerGui.py

--- managerGui.py
+++ managerGui.py
@@ -21,35 +21,35 @@ class managerGui():
 
         Label(text = ' The Number Of Female Visitors ',font='Times 15').grid(row=3,column=1,pady=10)
         self.femaleVisitors = Entry()
-        self.femaleVisitors.grid(row=2,column=2,columnspan=10)
+        self.femaleVisitors.grid(row=3,column=2,columnspan=10)
 
         Label(text = ' The Number Of Local Visitors ',font='Times 15').grid(row=4,column=1,pady=10)
         self.localVisitors = Entry()
-        self.localVisitors.grid(row=2,column=2,columnspan=10)
+        self.localVisitors.grid(row=4,column=2,columnspan=10)
 
         Label(text = ' The Number Of Tourists ',font='Times 15').grid(row=5,column=1,pady=10)
         self.tourists = Entry()
-        self.tourists.grid(row=2,column=2,columnspan=10)
+        self.tourists.grid(row=5,column=2,columnspan=10)
 
-        ttk.Button(text='REPORT',command=self.reportData).grid(row=3,column=2)
+        ttk.Button(text='REPORT',command=self.reportData).grid(row=6,column=2)
 
 
-    def loginUser(self):
+    def reportData(self):
         totVisitors = self.totVisitors.get()
         maleVisitors = self.maleVisitors.get() 
         femaleVisitors = self.femaleVisitors.get() 
         localVisitors = self.localVisitors.get() 
         tourists = self.tourists.get() 
-        if not totVisitors | not maleVisitors | not femaleVisitors | not localVisitors | not tourists: mb.showerror("Error", "Everything has to be filled out!")
+        if (not totVisitors or not maleVisitors or not femaleVisitors or not localVisitors or not tourists): mb.showerror("Error", "Everything has to be filled out!")
         else: 
             messageToServer = "rcrd" + ";" + totVisitors + ";" + maleVisitors + ";" + femaleVisitors + ";" + localVisitors + ";" + tourists #record message
             print("onclick: ", messageToServer)
             root.destroy()
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # this is to test the gui
 
     root = Tk()
-    root.geometry('425x225')
-    application = loginGui(root)
+    root.geometry('450x300')
+    application = managerGui(root)
 
     root.mainloop()
