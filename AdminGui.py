3bab8cb4e21de8e1f56ef8898613ca6d41fb3844
kidobeko@gmail.com <kidobeko@gmail.com>
2020-05-05 18:32:25 +0300

    Fix radio buttons

--- AdminGui.py
+++ AdminGui.py
@@ -6,10 +6,9 @@ import tkinter.messagebox as mb
 class AdminGui():
     
     def __init__(self,root,qMessage):
-        self.root = root
-        self.root.title('Admin')
-        global v 
-        v = tk.IntVar() #first one is seleceted initially
+        root.title('Admin')
+        self.v = tk.IntVar(root) #first one is seleceted initially
+        self.v.set(1)
         self.rbValues = {"The historical place with the maximum number of visitors": 1,
                         "The city with the maximum number of visitors": 2,
                         "Extended details for each city" : 3,
@@ -17,12 +16,12 @@ class AdminGui():
                         "Extended details for a historical place by date": 5}
 
         for (text, value) in self.rbValues.items(): 
-            tk.Radiobutton(self.root, text=text, variable=v, 
-                        value=value, state=tk.NORMAL).pack(side=tk.TOP, ipady=5)
-        self.bGenerateReport = tk.Button(self.root, text='CREATE REPORT',command= self.createQuery).pack()
+            tk.Radiobutton(root, text=text, variable=self.v, 
+                        value=value).pack(side=tk.TOP, ipady=5)
+        self.bGenerateReport = tk.Button(root, text='CREATE REPORT',command= self.createQuery).pack()
 
     def createQuery(self):
-        selection = v.get()
+        selection = self.v.get()
         print(selection)
         messageToServer = "adminQuery" + ";" + str(selection) #QUERY AND QUERY NUMBER message
         print("onclick: ", messageToServer)
